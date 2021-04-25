8a4e341abb5941bf00ae20d9ec4a469f0e3d1747
tienminh271 <tien.minh271@gmail.com>
2021-04-25 17:43:59 +0700

    Init

---
+++ src/test/java/com/ea/SpringBasic/SpringBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ea.SpringBasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
